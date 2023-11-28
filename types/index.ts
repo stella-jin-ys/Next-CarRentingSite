4c85901e6002155fc4f4a6ead370ee1fd212eddd
Stella <stella.jin123@gmail.com>
2023-11-28 14:33:07 +0100

    initial commit

---
+++ types/index.ts
@@ -0,0 +1,9 @@
+import { type } from "os";
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps {
+  title: string;
+  containerStyles?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: "button" | "submit";
+}
